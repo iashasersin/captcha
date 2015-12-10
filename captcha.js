2754589bf97f1f13d1249dcebe8839f35a9fadf1
root <manoperacraft@server1.manoperacraft.info>
2015-12-10 14:56:18 +0200

    backuped

--- captcha.js
+++ captcha.js
@@ -1,35 +1,46 @@
-var operators = [{
+
+	function randomNumbers () {
+		var operators = [{
 			sign: "+",
 			method: function(a,b){return a + b; }
-	     },
-         {
-	        sign:"&#215",
-	     	method: function(a,b){return a * b; }
-	     },
-	     {
+	     },{
 	     	sign:"-",
             method: function(a,b){return a - b; }
          },
          { sign:"&#247",
-            method: function(a,b){return a / b; }
-         }];
+            method: function(a,b){return a / b; }},
+         {
+	        sign:"&#215",
+	     	method: function(a,b){return a * b; }
+	     }];
 
-// var selectedOperator = randomNumber(operators.length);
-var x = randomNumber(11);
-var y = randomNumber(11);
+		var selectedOperator = Math.floor(Math.random() * operators.length);
 
-		if (x % y != 0 || x < y) {
-        var selectedOperator = Math.floor(Math.random() * 2);
-    }
-    	
-    	else 
-    	{var selectedOperator = Math.floor(Math.random() * operators.length);
-    };
-    
-var getSign = operators[selectedOperator].sign;
-var z = operators[selectedOperator].method(x,y);
+		var getSign = document.getElementById("sign");
+		getSign.innerHTML = operators[selectedOperator].sign;
 
 
-function randomNumber (a) {
-	return Math.floor(Math.random() * a);
-};
+	
+	var x = Math.floor(Math.random() * 11);
+	var y = Math.floor(Math.random() * 11);
+	z = operators[selectedOperator].method(x,y);
+	
+    var numbersList = ["zero ", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine ", "ten "]
+
+    var i = x;
+
+
+    var first = document.getElementById("firstNumber");
+    first.innerHTML = numbersList[i];
+    var second = document.getElementById("secondNumber");
+    second.innerHTML = y;
+}
+    function getResult () {
+    var result = document.getElementById("result");
+    var w = result.value;
+    if (w == z) {alert("corect");}
+    else if (w != z) {alert("wrong");}
+}    
+
+
+window.onload = randomNumbers;
