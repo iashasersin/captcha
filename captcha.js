9e71e1e4cbcfc2925ae6e42310f25f8692195ba3
root <manoperacraft@server1.manoperacraft.info>
2015-12-10 14:33:06 +0200

    added GATA

--- captcha.js
+++ captcha.js
@@ -1,46 +1,35 @@
-
-	function randomNumbers () {
-		var operators = [{
+var operators = [{
 			sign: "+",
 			method: function(a,b){return a + b; }
-	     },{
+	     },
+         {
+	        sign:"&#215",
+	     	method: function(a,b){return a * b; }
+	     },
+	     {
 	     	sign:"-",
             method: function(a,b){return a - b; }
          },
          { sign:"&#247",
-            method: function(a,b){return a / b; }},
-         {
-	        sign:"&#215",
-	     	method: function(a,b){return a * b; }
-	     }];
+            method: function(a,b){return a / b; }
+         }];
 
-		var selectedOperator = Math.floor(Math.random() * operators.length);
+// var selectedOperator = randomNumber(operators.length);
+var x = randomNumber(11);
+var y = randomNumber(11);
 
-		var getSign = document.getElementById("sign");
-		getSign.innerHTML = operators[selectedOperator].sign;
-
-
-	
-	var x = Math.floor(Math.random() * 11);
-	var y = Math.floor(Math.random() * 11);
-	z = operators[selectedOperator].method(x,y);
-	
-    var numbersList = ["zero ", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", "nine ", "ten "]
-
-    var i = x;
+		if (x % y != 0 || x < y) {
+        var selectedOperator = Math.floor(Math.random() * 2);
+    }
+    	
+    	else 
+    	{var selectedOperator = Math.floor(Math.random() * operators.length);
+    };
+    
+var getSign = operators[selectedOperator].sign;
+var z = operators[selectedOperator].method(x,y);
 
 
-    var first = document.getElementById("firstNumber");
-    first.innerHTML = numbersList[i];
-    var second = document.getElementById("secondNumber");
-    second.innerHTML = y;
-}
-    function getResult () {
-    var result = document.getElementById("result");
-    var w = result.value;
-    if (w == z) {alert("corect");}
-    else if (w != z) {alert("wrong");}
-}    
-
-
-window.onload = randomNumbers;
+function randomNumber (a) {
+	return Math.floor(Math.random() * a);
+};
